13e9fd70ddcb3c4d7e4d4c65f386c3a0e2bb27e9
NDM-sumin <ducminhs2tb@gmail.com>
2023-12-20 22:46:06 +0700

    fix odata errror on create

--- app/api/Controllers/Base/AppCRUDController.cs
+++ app/api/Controllers/Base/AppCRUDController.cs
@@ -18,9 +18,9 @@ namespace api.Controllers.Base
         }
 
         [HttpGet]
-        public virtual IActionResult GetAll()
+        public virtual async Task<IActionResult> GetAll()
         {
-            return Ok(appCRUDService.GetQueryable());
+            return Ok(await appCRUDService.GetQueryable());
         }
 
         [HttpPost]
