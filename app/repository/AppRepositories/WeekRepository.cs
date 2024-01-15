2d3585c1d4567f14c8cf5ec171f581ec6506c4cf
quan1701 <105469254+quan1701@users.noreply.github.com>
2024-01-15 21:42:19 +0700

    Feature/student (#19)
    * student timetable
    
    * timetable
    
    * fix conflict
    
    * fix code
    
    * fix code
    
    * score history
    
    * score history
    
    * teacherTimetable
    
    * fix bug
    
    * teacher attendance
    
    * fix conflict
    
    * fix table score
    
    * fix score manager
    
    * fee history
    
    * generate timetable

---
+++ app/repository/AppRepositories/WeekRepository.cs
@@ -0,0 +1,19 @@
+﻿using domain;
+using entityframework;
+using repository.AppRepositories.Base;
+using repository.contract.IAppRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace repository.AppRepositories
+{
+    public class WeekRepository : AppGenericDefaultKeyRepository<Week>, IWeekRepository
+    {
+        public WeekRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
